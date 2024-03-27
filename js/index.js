bdd299cf0ac47ae8336d0bd055c7643687000bc7
evalsam2003 <esaimoke@gmail.com>
2024-03-27 12:03:51 +0100

    variables and data types

---
+++ js/index.js
@@ -0,0 +1,45 @@
+document.write ("Welcome to the digital world");
+const nameOne ="sam"
+//string
+
+//this sis for storung alphanumeric
+const name = "gabriel"
+
+//number
+let age = 45;
+
+//undefined
+let choice = undefined
+
+//null
+let pick = null;
+
+//boelean ( true or false)
+const check = true
+
+//object
+const obj = {
+    name :"gabriel",
+    age: 45,
+    pick:null,
+    check: false, 
+    choice:undefined
+} 
+console.log (obj.age)
+
+//array(indexs)
+const array = [
+    "gabriel",
+    "dame",
+    45,
+    60
+]
+console.log(array[1])
+
+//functions (for specifying tasks or jobs)
+function functionName () {
+    return "this is a function"
+}
+
+//calling a function
+console.log (functionName())
